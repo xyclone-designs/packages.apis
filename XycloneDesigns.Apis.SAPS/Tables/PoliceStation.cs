862591737b4a761ef76a5f7eb96ea57530564088
Thando Moeketsi Ndlovu <thandomndlovu@gmail.com>
2025-07-02 21:13:50 +0200

    General Updates

--- XycloneDesigns.Apis.SAPS/Tables/PoliceStation.cs
+++ XycloneDesigns.Apis.SAPS/Tables/PoliceStation.cs
@@ -10,6 +10,7 @@ namespace XycloneDesigns.Apis.SAPS.Tables
 
 			public const string Column_Name = "name";
 			public const string Column_List_PkRecord = "list_pkRecord";
+			public const string Column_PkDistrict = "pkDistrict";
 			public const string Column_PkMunicipality = "pkMunicipality";
             public const string Column_PkProvince = "pkProvince";
         }
@@ -17,6 +18,7 @@ namespace XycloneDesigns.Apis.SAPS.Tables
 		[SQLite.Unique]
 		[SQLite.Column(SQL.Column_Name)] public string? Name { get; set; }
         [SQLite.Column(SQL.Column_List_PkRecord)] public string? List_PkRecord { get; set; }
+        [SQLite.Column(SQL.Column_PkDistrict)] public int? PkDistrict { get; set; }
         [SQLite.Column(SQL.Column_PkMunicipality)] public int? PkMunicipality { get; set; }
         [SQLite.Column(SQL.Column_PkProvince)] public int? PkProvince { get; set; }        
     }
